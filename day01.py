6c845608aae640d4be6b3ddd396c8e86806af60b
AshIsbitt <ashisbitt@icloud.com>
2021-12-01 09:10:28 +0000

    Day 1 part 1 complete

---
+++ day01.py
@@ -0,0 +1,52 @@
+# What to do
+import pytest
+
+
+def depthIncreaseDetector(depthData:list[int]) -> int:
+    counter:int = 0
+
+    for itera, item in enumerate(depthData):
+        if depthData[itera] > depthData[itera-1]:
+            counter +=1
+            
+    return counter
+
+
+def main(filename:str) -> int:
+    with open(filename) as inputData:
+        rawData = inputData.readlines()
+
+    depthData = list(map(int, rawData))
+
+    depthCounter = depthIncreaseDetector(depthData)
+    print(f'Part 1: {depthCounter}')
+    return 0
+
+
+if __name__ == '__main__':
+    # raise SystemExit(main('input_ff/day01.txt'))
+    raise SystemExit(main('input_sri/day01.txt'))
+
+
+#Tests
+
+test_data = [199,
+200,
+208,
+210,
+200,
+207,
+240,
+269,
+260,
+263]
+
+@pytest.mark.parametrize(
+    ("input_data", "expected"),
+    (
+        (test_data, 7),
+    )
+)
+def test_depthIncreaseDetector(input_data:list[int], expected:int) -> None:
+    assert depthIncreaseDetector(input_data) == expected
+
