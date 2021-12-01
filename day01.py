802f556803e6faf152c10137d0180184d4a6783c
AshIsbitt <ashisbitt@icloud.com>
2021-12-01 09:13:28 +0000

    day 1 pre-commit

--- day01.py
+++ day01.py
@@ -2,51 +2,37 @@
 import pytest
 
 
-def depthIncreaseDetector(depthData:list[int]) -> int:
-    counter:int = 0
+def depthIncreaseDetector(depthData: list[int]) -> int:
+    counter: int = 0
 
     for itera, item in enumerate(depthData):
-        if depthData[itera] > depthData[itera-1]:
-            counter +=1
-            
+        if depthData[itera] > depthData[itera - 1]:
+            counter += 1
+
     return counter
 
 
-def main(filename:str) -> int:
+def main(filename: str) -> int:
     with open(filename) as inputData:
         rawData = inputData.readlines()
 
     depthData = list(map(int, rawData))
 
     depthCounter = depthIncreaseDetector(depthData)
-    print(f'Part 1: {depthCounter}')
+    print(f"Part 1: {depthCounter}")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # raise SystemExit(main('input_ff/day01.txt'))
-    raise SystemExit(main('input_sri/day01.txt'))
+    raise SystemExit(main("input_sri/day01.txt"))
 
 
-#Tests
+# Tests
 
-test_data = [199,
-200,
-208,
-210,
-200,
-207,
-240,
-269,
-260,
-263]
+test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-@pytest.mark.parametrize(
-    ("input_data", "expected"),
-    (
-        (test_data, 7),
-    )
-)
-def test_depthIncreaseDetector(input_data:list[int], expected:int) -> None:
+
+@pytest.mark.parametrize(("input_data", "expected"), ((test_data, 7),))
+def test_depthIncreaseDetector(input_data: list[int], expected: int) -> None:
     assert depthIncreaseDetector(input_data) == expected
-
