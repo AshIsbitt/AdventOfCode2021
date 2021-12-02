dbacc3ed7a3ff2a98a1d10cc4fd1b30a0ba8c1aa
AshIsbitt <ashisbitt@icloud.com>
2021-12-02 08:58:13 +0000

    Day 2 part 1 complete

---
+++ day02.py
@@ -0,0 +1,43 @@
+# What to do
+import pytest
+
+
+def calcPosition(posData: list[str]) -> int:
+    horizontal_pos: int = 0
+    vertical_pos: int = 0
+
+    for item in posData:
+        moveData = item.split(" ")
+        if moveData[0] == "forward":
+            horizontal_pos += int(moveData[1])
+        elif moveData[0] == "up":
+            vertical_pos -= int(moveData[1])
+        elif moveData[0] == "down":
+            vertical_pos += int(moveData[1])
+
+    return horizontal_pos * vertical_pos
+
+
+def main(filename: str) -> int:
+    with open(filename) as inputData:
+        rawData = inputData.readlines()
+
+    # data = list(map(int, rawData))
+    current_pos = calcPosition(rawData)
+    print(f"Part 1: {current_pos}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    # raise SystemExit(main("input_ff/day02.txt"))
+    raise SystemExit(main("input_sri/day02.txt"))
+
+
+# Tests
+test_data = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+
+
+@pytest.mark.parametrize(("input_data", "expected"), ((test_data, 150),))
+def test_calcPosition(input_data: list[str], expected: int) -> None:
+    assert calcPosition(input_data) == expected
