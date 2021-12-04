2e0a1fef3543ef25869dd362d819df1ba76608ba
AshIsbitt <ashisbitt@icloud.com>
2021-12-04 16:55:24 +0000

    Day 4 part 1 with failing tests updated

--- day04.py
+++ day04.py
@@ -1,5 +1,5 @@
 # To guarantee victory against the giant squid, figure out which board will win
-# first. What will your final score be if you choose that board?#
+# first. What will your final score be if you choose that board?
 import pprint
 
 import pytest
