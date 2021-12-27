ef17238cd2af142ce60671a030dffe014ef30579
Ttibsi <ashisbitt@icloud.com>
2021-12-27 21:22:22 +0000

    Day 8 part 2 started

--- day08.py
+++ day08.py
@@ -1,4 +1,9 @@
 # Part 1: In the output values, how many times do digits 1, 4, 7, or 8 appear?
+# Part 2: For each entry, determine all of the wire/segment connections and
+# decode the four-digit output values. What do you get if you add up all of the
+# output values?
+from collections import defaultdict
+
 import pytest
 
 
@@ -32,11 +37,77 @@ def identify_unique_segments(rawData: list[str]) -> int:
     return unique_segments
 
 
+def segment_calculator(line: list[str]) -> dict[int, str]:
+    words = line[0].split(" ")
+
+    word_strings = defaultdict(str)
+
+    for word in words:
+        word = "".join(sorted(word))
+
+        if len(word) == 3:
+            word_strings[7] = word
+        elif len(word) == 4:
+            word_strings[4] = word
+        elif len(word) == 2:
+            word_strings[1] = word
+        elif len(word) == 7:
+            word_strings[8] = word
+
+    # This only works when the other loop is complete
+    for word in words:
+        if len(word) == 5 and word_strings[7] in word:
+            word_strings[3] = word
+
+    for word in words:
+        if word_strings[4] in word and word_strings[3] in word:
+            word_strings[9] = word
+        if len(word) == 6 and word_strings[7] in word:
+            word_strings[0] = word
+        if len(word) == 6 and not word_strings[7] in word:
+            word_strings[6] = word
+
+    for word in words:
+        if word == word_strings[6]:
+            word_strings[5] = word
+        if word not in word_strings.values():
+            word_strings[2] = word
+
+    return word_strings
+
+
+def calculate_output_vals(rawData: list[str]) -> int:
+    parsed_data = parse_records(rawData)
+    total_output = 0
+
+    for line in parsed_data:
+        segs = segment_calculator(line)
+        print(segs)
+        num = "0"
+
+        # get output side
+        output = line[1]
+
+        # Calc nums from segs.values()
+        for word in output:
+            word = "".join(sorted(word))
+
+            for k, v in segs.items():
+                if word in v:
+                    num += str(k)
+
+        total_output += int(num)
+        print(total_output)
+
+    return total_output
+
+
 def main(filename: str) -> int:
     with open(filename) as inputData:
         rawData = inputData.readlines()
 
     print(f"Part 1: {identify_unique_segments(rawData)}")
+    print(f"Part 2: {calculate_output_vals(rawData)}")
 
     return 0
 
@@ -77,6 +148,12 @@ def test_identify_unique_segments(input_data: list[str], expected: int) -> None:
 
 
 # Part 2 test
-# @pytest.mark.parametrize(("input_data", "expected"), ((test_data, 0),))
-# def test_f(input_data: list[str], expected: int) -> None:
-#    assert f(input_data) == expected
+@pytest.mark.parametrize(
+    ("input_data", "expected"),
+    (
+        (test_data_1, 5353),
+        (test_data_2, 61229),
+    ),
+)
+def test_calculate_output_vals(input_data: list[str], expected: int) -> None:
+    assert calculate_output_vals(input_data) == expected
