6d439b077d26f4bae83d29905956c3d32d0476be
Ttibsi <ashisbitt@icloud.com>
2022-01-01 23:14:11 +0000

    Updated blank aoc template

--- blank.py
+++ blank.py
@@ -10,12 +10,15 @@ def main(filename: str) -> int:
     # Convert to ints
     # data = list(map(int, rawData))
 
+    print(f"Part 1: {}")
+    #print(f"Part 2: {}")
+
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main("input_ff/day1.txt"))
-    # raise SystemExit(main('input_sri/day1.txt'))
+    #raise SystemExit(main("input_ff/day01.txt"))
+    raise SystemExit(main('input_sri/day01.txt'))
 
 
 # Tests
@@ -29,6 +32,6 @@ def test_f(input_data: list[int], expected: int) -> None:
 
 
 # Part 2 test
-@pytest.mark.parametrize(("input_data", "expected"), ((test_data, 0),))
-def test_f(input_data: list[int], expected: int) -> None:
-    assert f(input_data) == expected
+#@pytest.mark.parametrize(("input_data", "expected"), ((test_data, 0),))
+#def test_f(input_data: list[int], expected: int) -> None:
+#    assert f(input_data) == expected
