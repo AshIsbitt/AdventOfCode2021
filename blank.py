9437f3185afa9eca8ba35fbf6013366db22978ee
Ttibsi <ashisbitt@icloud.com>
2022-01-08 17:43:53 +0000

    Blank.py updated with more functional boilerplate

--- blank.py
+++ blank.py
@@ -1,6 +1,6 @@
 # What to do
+import pyperclip as pyp  # type: ignore
 import pytest
-import pyperclip as pyp # type: ignore
 
 
 def main(filename: str) -> int:
@@ -11,33 +11,29 @@ def main(filename: str) -> int:
     # Convert to ints
     # data = list(map(int, rawData))
 
-    if 'sri' in filename:
-        print('Browser: Safari')
-    elif 'ff' in filename:
-        print('Browser: Firefox')
+    if "sri" in filename:
+        print("Browser: Safari")
+    elif "ff" in filename:
+        print("Browser: Firefox")
 
-    p1 = pass
+    p1 = 0
+    pyp.copy(p1)
     print(f"Part 1: {p1}")
-    #p2 = pass
-    #print(f"Part 2: {p2}")
 
-    try:
-        pyp.copy(p2)
-        print('Copied: Part 2')
-    except NameError:
-        pyp.copy(p1)
-        print('Copied: Part 1')
+    # p2 = 0
+    # pyp.copy(p2)
+    # print(f"Part 2: {p2}")
 
     return 0
 
 
 if __name__ == "__main__":
-    #raise SystemExit(main("input_ff/day01.txt"))
-    raise SystemExit(main('input_sri/day01.txt'))
+    # raise SystemExit(main("input_ff/day01.txt"))
+    raise SystemExit(main("input_sri/day01.txt"))
 
 
 # Tests
-test_data = []
+test_data: list[int] = []
 
 
 # Part 1 test
@@ -45,7 +41,7 @@ test_data = []
     ("input_data", "expected"),
     [
         (test_data, 0),
-    ]
+    ],
 )
 def test_f(input_data: list[int], expected: int) -> None:
     assert f(input_data) == expected
