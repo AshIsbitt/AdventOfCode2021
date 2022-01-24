95a9982174b8f3b334fc0fb91a3ece45e3343cca
Ttibsi <ashisbitt@icloud.com>
2022-01-24 09:11:58 +0000

    Day 13 set up with a test

--- day13.py
+++ day13.py
@@ -1,22 +1,31 @@
-# What to do
+# Part 1: How many dots are visible after completing just the first fold
+# instruction on your transparent paper?
 import pyperclip as pyp  # type: ignore
 import pytest
 
 
+def parse_input(rawData: str) -> int:
+    pass
+
+
+# Part 1
+def first_fold(rawData: str) -> int:
+    data = parse_input(rawData)
+    print(data)
+
+    return data
+
+
 def main(filename: str) -> int:
     with open(filename) as inputData:
-        rawData = inputData.readlines()
-    rawData = [line.rstrip("\n") for line in rawData]
-
-    # Convert to ints
-    # data = list(map(int, rawData))
+        rawData = inputData.read()
 
     if "sri" in filename:
         print("Browser: Safari")
     elif "ff" in filename:
         print("Browser: Firefox")
 
-    p1 = 0
+    p1 = first_fold(rawData)
     pyp.copy(p1)
     print(f"Part 1: {p1}")
 
@@ -28,23 +37,43 @@ def main(filename: str) -> int:
 
 
 if __name__ == "__main__":
-    # raise SystemExit(main("input_ff/day01.txt"))
-    raise SystemExit(main("input_sri/day01.txt"))
+    # raise SystemExit(main("input_ff/day13.txt"))
+    raise SystemExit(main("input_sri/day13.txt"))
 
 
 # Tests
-test_data: list[int] = []
+test_data = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"""
 
 
 # Part 1 test
 @pytest.mark.parametrize(
     ("input_data", "expected"),
     [
-        (test_data, 0),
+        (test_data, 17),
     ],
 )
-def test_f(input_data: list[int], expected: int) -> None:
-    assert f(input_data) == expected
+def test_first_fold(input_data, expected) -> None:
+    assert first_fold(input_data) == expected
 
 
 # Part 2 test
@@ -55,6 +84,6 @@ def test_f(input_data: list[int], expected: int) -> None:
         (test_data, 0),
     ]
 )
-def test_f(input_data: list[int], expected: int) -> None:
+def test_f(input_data, expected) -> None:
     assert f(input_data) == expected
 """
