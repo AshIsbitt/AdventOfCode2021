46756a402c0a78e4208aa71202e596888f3719c5
Ttibsi <ashisbitt@icloud.com>
2022-01-30 15:41:11 +0000

    Day 15 blank clone

---
+++ day15.py
@@ -0,0 +1,64 @@
+# What to do
+import pprint as p
+from collections import counter
+from collections import defaultdict
+
+import pyperclip as pyp  # type: ignore
+import pytest
+
+
+def main(filename: str) -> int:
+    with open(filename) as inputData:
+        rawData = inputData.readlines()
+    rawData = [line.rstrip("\n") for line in rawData]
+
+    # Convert to ints
+    # data = list(map(int, rawData))
+
+    if "sri" in filename:
+        print("Browser: Safari")
+    elif "ff" in filename:
+        print("Browser: Firefox")
+
+    p1 = 0
+    pyp.copy(p1)
+    print(f"Part 1: {p1}")
+
+    # p2 = 0
+    # pyp.copy(p2)
+    # print(f"Part 2: {p2}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    # raise SystemExit(main("input_ff/day15.txt"))
+    raise SystemExit(main("input_sri/day15.txt"))
+
+
+# Tests
+test_data: list[int] = []
+
+
+# Part 1 test
+@pytest.mark.parametrize(
+    ("input_data", "expected"),
+    [
+        (test_data, 0),
+    ],
+)
+def test_f(input_data: list[int], expected: int) -> None:
+    assert f(input_data) == expected
+
+
+# Part 2 test
+"""
+@pytest.mark.parametrize(
+    ("input_data", "expected"),
+    [
+        (test_data, 0),
+    ]
+)
+def test_f(input_data: list[int], expected: int) -> None:
+    assert f(input_data) == expected
+"""
