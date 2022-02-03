2b4a96822146398ce154f776897a501be5003ba6
Ttibsi <ashisbitt@icloud.com>
2022-02-03 13:25:21 +0000

    Day 16 part 1 tests

--- day16.py
+++ day16.py
@@ -1,10 +1,16 @@
-# What to do
+# Part 1: Decode the structure of your hexadecimal-encoded BITS transmission;
+# what do you get if you add up the version numbers in all packets?
 import pprint as p
 from collections import Counter
 from collections import defaultdict
 
 import pyperclip as pyp  # type: ignore
 import pytest
+
+
+# Part 1
+def packet_decoder(data: str) -> int:
+    pass
 
 
 def main(filename: str) -> int:
@@ -16,7 +22,7 @@ def main(filename: str) -> int:
     elif "ff" in filename:
         print("Browser: Firefox")
 
-    p1 = 0
+    p1 = packet_decoder(raw_data)
     pyp.copy(p1)
     print(f"Part 1: {p1}")
 
@@ -28,24 +34,23 @@ def main(filename: str) -> int:
 
 
 if __name__ == "__main__":
-    # raise SystemExit(main("input_ff/day01.txt"))
-    raise SystemExit(main("input_sri/day01.txt"))
+    # raise SystemExit(main("input_ff/day16.txt"))
+    raise SystemExit(main("input_sri/day16.txt"))
 
 
 # Tests
-test_data = """
-"""
-
-
 # Part 1 test
 @pytest.mark.parametrize(
     ("input_data", "expected"),
     [
-        (test_data, 0),
+        ("8A004A801A8002F478", 16),
+        ("620080001611562C8802118E34", 12),
+        ("C0015000016115A2E0802F182340", 23),
+        ("A0016C880162017C3686B18A3D4780", 31),
     ],
 )
-def test_f(input_data, expected):
-    assert f(input_data) == expected
+def test_packet_decoder(input_data, expected):
+    assert packet_decoder(input_data) == expected
 
 
 # Part 2 test
