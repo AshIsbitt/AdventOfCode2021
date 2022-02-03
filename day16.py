1a9b7a5fe97b3f6b5736ed081a0c220d6a93e5d5
Ttibsi <ashisbitt@icloud.com>
2022-02-03 13:37:42 +0000

    Day 16 part 1 convert to binary

--- day16.py
+++ day16.py
@@ -8,9 +8,42 @@ import pyperclip as pyp  # type: ignore
 import pytest
 
 
+TRANSLATION_TABLE = {
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    "A": "1010",
+    "B": "1011",
+    "C": "1100",
+    "D": "1101",
+    "E": "1110",
+    "F": "1111",
+}
+
+
+def hex_to_bin(msg: str) -> str:
+    new_msg = ""
+    for char in msg.strip():
+        new_msg += TRANSLATION_TABLE[char]
+
+    while new_msg[-1] == "0":
+        new_msg = new_msg[:-1]
+
+    return new_msg
+
+
 # Part 1
 def packet_decoder(data: str) -> int:
-    pass
+    binary_string = hex_to_bin(data)
+    print(binary_string)
+    return 0
 
 
 def main(filename: str) -> int:
