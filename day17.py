f6812597faa57ad59af95901813d7b8cae520c49
Ttibsi <ashisbitt@icloud.com>
2022-02-16 14:12:10 +0000

    Day 17 changed parsed data to a dict

--- day17.py
+++ day17.py
@@ -9,19 +9,23 @@ import pyperclip as pyp  # type: ignore
 import pytest
 
 
-def parse_input(data: str) -> tuple[tuple[int, int], tuple[int, int]]:
-    x, y = (data.split(": "))[1].split(", ")
+def parse_input(inp: str) -> dict[str, int]:
+    x, y = (inp.split(": "))[1].split(", ")
     x_s, x_e = x.split("..")
     y_s, y_e = y.split("..")
-    return ((int(x_s[2:]), int(x_e)), (int(y_s[2:]), int(y_e)))
+
+    data: dict[str, int] = {}
+    data["x_start"] = int(x_s[2:])
+    data["x_end"] = int(x_e)
+    data["y_start"] = int(y_s[2:])
+    data["y_end"] = int(y_e)
+    return data
 
 
-def check_trajectory(
-    x: int, y: int, targets: tuple[tuple[int, int], tuple[int, int]]
-) -> int:
+def check_trajectory(x: int, y: int, targets: dict[str, int]) -> int:
 
-    x_pos = x
-    y_pos = y
+    x_pos = 0
+    y_pos = 0
     highest_point = 0
     success = False
 
@@ -29,21 +33,23 @@ def check_trajectory(
         x_pos += x
         y_pos += y
 
-        if x_pos > 0:
-            x_pos -= 1
-        elif x_pos < 0:
-            x_pos += 1
+        if x > 0:
+            x -= 1
+        elif x < 0:
+            x += 1
 
-        y_pos -= 1
+        y -= 1
 
-        print(x_pos, y_pos)
         if y_pos < highest_point:
             highest_point = y_pos
 
-        if x_pos in range(*targets[0]) and y_pos in range(*targets[1]):
+        if x_pos in range(targets["x_start"], targets["x_end"]) and y_pos in range(
+            targets["y_start"], targets["y_end"]
+        ):
+
             success = True
             break
-        elif x_pos > targets[0][1] or y_pos > targets[1][1]:
+        elif x_pos > targets["x_end"] or y_pos > targets["y_end"]:
             break
 
     if success:
@@ -56,16 +62,15 @@ def get_highest_value(valid: dict[tuple[int, int], int]) -> tuple[int, int]:
     return max(valid, key=lambda key: valid[key])
 
 
-def trajectory_iterator(targets: tuple[tuple[int, int], tuple[int, int]]) -> int:
+def trajectory_iterator(targets: dict[str, int]) -> int:
     valid: dict[tuple[int, int], int] = {}
 
-    RANGE_CHECK = 25
+    RANGE_CHECK = 2500
     for x in range(1, RANGE_CHECK):
-        for y in range(1, RANGE_CHECK):
+        for y in range(-RANGE_CHECK, RANGE_CHECK):
             # returns an int with it's highest y value or 0 if it's not within target area
             highest_point = check_trajectory(x, y, targets)
 
-            print(highest_point)
             if highest_point != 0:
                 valid[(x, y)] = highest_point
 
