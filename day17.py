6825efd03ec83b6f2347bdd0b23acc72eb91be52
Ttibsi <ashisbitt@icloud.com>
2022-02-16 12:35:11 +0000

    Day 17 while loop

--- day17.py
+++ day17.py
@@ -19,25 +19,57 @@ def parse_input(data: str) -> tuple[tuple[int, int], tuple[int, int]]:
 def check_trajectory(
     x: int, y: int, targets: tuple[tuple[int, int], tuple[int, int]]
 ) -> int:
-    ...
+
+    x_pos = x
+    y_pos = y
+    highest_point = 0
+    success = False
+
+    while True:
+        x_pos += x
+        y_pos += y
+
+        if x_pos > 0:
+            x_pos -= 1
+        elif x_pos < 0:
+            x_pos += 1
+
+        y_pos -= 1
+
+        print(x_pos, y_pos)
+        if y_pos < highest_point:
+            highest_point = y_pos
+
+        if x_pos in range(*targets[0]) and y_pos in range(*targets[1]):
+            success = True
+            break
+        elif x_pos > targets[0][1] or y_pos > targets[1][1]:
+            break
+
+    if success:
+        return highest_point
+    else:
+        return 0
 
 
-def get_highest_value(valid: dict[tuple[int, int], int]) -> int:
-    ...
+def get_highest_value(valid: dict[tuple[int, int], int]) -> tuple[int, int]:
+    return max(valid, key=lambda key: valid[key])
 
 
 def trajectory_iterator(targets: tuple[tuple[int, int], tuple[int, int]]) -> int:
     valid: dict[tuple[int, int], int] = {}
 
-    for x in range(1, 10):
-        for y in range(1, 10):
+    RANGE_CHECK = 25
+    for x in range(1, RANGE_CHECK):
+        for y in range(1, RANGE_CHECK):
             # returns an int with it's highest y value or 0 if it's not within target area
             highest_point = check_trajectory(x, y, targets)
 
-            if highest_point > 0:
+            print(highest_point)
+            if highest_point != 0:
                 valid[(x, y)] = highest_point
 
-    highest_val = get_highest_value(valid)
+    highest_val = valid[get_highest_value(valid)]
     return highest_val
 
 
