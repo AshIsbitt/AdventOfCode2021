c173ab26827783f3483df2389c9781b21848262d
Ttibsi <ashisbitt@icloud.com>
2022-02-22 09:19:29 +0000

    Day 18A unit test

--- day18.py
+++ day18.py
@@ -1,10 +1,16 @@
-# What to do
+# Part 1:Add up all of the snailfish numbers from the homework assignment in
+# the order they appear. What is the magnitude of the final sum?
 import pprint as p
 from collections import Counter
 from collections import defaultdict
 
 import pyperclip as pyp  # type: ignore
 import pytest
+
+
+# Part 1
+def number_homework(raw_data) -> int:
+    ...
 
 
 def main(filename: str) -> int:
@@ -16,7 +22,7 @@ def main(filename: str) -> int:
     elif "ff" in filename:
         print("Browser: Firefox")
 
-    p1 = 0
+    p1 = number_homework(raw_data)
     pyp.copy(p1)
     print(f"Part 1: {p1}")
 
@@ -28,12 +34,22 @@ def main(filename: str) -> int:
 
 
 if __name__ == "__main__":
-    # raise SystemExit(main("input_ff/day01.txt"))
-    raise SystemExit(main("input_sri/day01.txt"))
+    # raise SystemExit(main("input_ff/day18.txt"))
+    raise SystemExit(main("input_sri/day18.txt"))
 
 
 # Tests
 test_data = """
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
 """
 
 
@@ -41,11 +57,11 @@ test_data = """
 @pytest.mark.parametrize(
     ("input_data", "expected"),
     [
-        (test_data, 0),
+        (test_data, 4140),
     ],
 )
-def test_f(input_data, expected):
-    assert f(input_data) == expected
+def test_number_homework(input_data, expected):
+    assert number_homework(input_data) == expected
 
 
 # Part 2 test
