6bc63c2b44efab023594e21b5516081c6159c96d
Ash <ashisbitt@icloud.com>
2022-03-12 11:11:16 +0000

    GitHub update - started adding part 1 tests again

--- day18B.py
+++ day18B.py
@@ -200,3 +200,23 @@ def main(filename: str) -> int:
 if __name__ == "__main__":
     # raise SystemExit(main("input_ff/day18.txt"))
     raise SystemExit(main("input_sri/day18.txt"))
+
+test_data = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"""
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    (
+        (test_data, 4140),
+    )
+)
+def test_calculate_homework(input:str, expected:int) -> None:
+    assert calculate_homework(input) == expected
